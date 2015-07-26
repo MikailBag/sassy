9e6db494789853b33d9e46af922a51357d2904b2
michaelbag <bagishov.mikail@ya.ru>
2015-07-26 15:21:34 +0300

    uncomment features added (i think fully)

--- sassy-core/main.js
+++ sassy-core/main.js
@@ -1,15 +1,42 @@
 var sassy = (function () {
     var sassy;
-    sassy={};
 
-    function compile(input/*, output*/) {
-        console.log(input);
-        input = input.replace(/\/\/.*/gm, '');// deleting // comments
-        input = input.replace(/\/\*[^!].*?\*\//gm, '');// deleting /**/ comments
-        return input;
+    function cut(str, startPos, endPos) {
+        console.log('cutting str ' + str);
+        var chars = str.split('');
+        console.log('cutted substr ' + chars.splice(startPos, endPos - startPos + 2));
+        return chars.join('')
+
+
     }
 
-    sassy.compile = compile;
+    function uncomment(str) {
+        str = str.replace(/\/\/.*/gm, '');// deleting // comments
+        var index = str.indexOf('/*');
+
+
+        if (index == -1) {
+            console.log('no multiline');
+        } else {
+            var endIndex = str.indexOf('*/', index);
+            console.log('multiline found;\nstart index: ' + index + ' end index: ' + endIndex);
+            str = cut(str, index, endIndex);
+        }
+
+        return str;
+    }
+
+    function compile(str/*, output*/) {
+        //console.log(str);
+        str = str.replace(/\/\/.*/gm, '');// deleting // comments
+        str = str.replace(/\/\*[^!].*?\*\//gm, '');// deleting /**/ comments
+        return str;
+    }
+
+    sassy = {
+        compile: compile,
+        uncomment: uncomment
+    };
     return sassy;
 
 
--- sassy-core/tests/tests.js
+++ sassy-core/tests/tests.js
@@ -1,8 +1,33 @@
 console.log('tests included');
-console.log(QUnit, compile);
+console.log(QUnit, sassy.compile);
 QUnit.test("hello test", function (assert) {
     assert.ok(1 == "1", "Passed!");
 });
-QUnit.test('api is availible',function(assert){
-    assert.ok(compile,'compiler availible');
+QUnit.test('api is availible', function (assert) {
+    assert.ok(sassy.compile, 'Compiler availible');
+    assert.ok(sassy.uncomment, 'Comment clean system');
+});
+
+QUnit.test('Comment clean system', function (assert) {
+    var str, res;
+    str = 'text' +
+        '\n//comment' +
+        '\n//comment' +
+        '\nin-str //comment' +
+        '\ntext';
+    res = 'text' +
+        '\n\n\nin-str ' +
+        '\ntext';
+    assert.equal(sassy.uncomment(str), res, 'delets // comments');
+    str = 'text' +
+        '\n//comment' +
+        '\n//comment' +
+        '\nin-str //comment' +
+        '\n/*multiline comment*/' +
+        '\ntext';
+    res = 'text' +
+        '\n\n\nin-str ' +
+        '\n\ntext';
+    assert.equal(sassy.uncomment(str), res, 'delets /**/ comments');
+
 });
